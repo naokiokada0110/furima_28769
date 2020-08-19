a528164b947498f11d1d9b74f91e6b741c8d6b8e
naokiokada0110 <okdman.agi@gmail.com>
2020-08-19 19:17:00 +0900

    新規登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases
+
+  with_options presence: true do
+    validates :email, format: { with: /@./, message: "このメールアドレスは使用できません。"}
+    validates :password, format: { with: /\A[a-zA-Z0-9]+\z{6,}/, message: "is invalid. Input full-width characters."}
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid Input half-width characters."}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'products', to: 'products#index'
+  root 'products#index'
 end
