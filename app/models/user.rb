9abf7fc1dd3a8a89e4a24b90ecdfaa0d5b8398f0
naokiokada0110 <okdman.agi@gmail.com>
2020-08-20 20:01:59 +0900

    バリデーション修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,18 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
+  PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z{6,}/.freeze
+  EMAIL_REGEX = /@./.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
   with_options presence: true do
     validates :nickname, :birthday
-    validates :email, format: { with: /@./, message: 'このメールアドレスは使用できません。' }
-    validates :password, format: { with: /\A[a-zA-Z0-9]+\z{6,}/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid Input half-width characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid Input half-width characters.' }
+    validates :email, format: { with: EMAIL_REGEX, message: 'このメールアドレスは使用できません。' }
+    validates :password, format: { with: PASSWORD_REGEX, message: 'is invalid. Input full-width characters.' }
+    validates :last_name, format: { with: NAME_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name, format: { with: NAME_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_kana, format: { with: NAME_KANA_REGEX, message: 'is invalid Input half-width characters.' }
+    validates :first_name_kana, format: { with: NAME_KANA_REGEX, message: 'is invalid Input half-width characters.' }
   end
 end
