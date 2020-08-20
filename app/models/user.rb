136ffd6f73a598ed409421379ef89e99901ad6a9
naokiokada0110 <okdman.agi@gmail.com>
2020-08-20 18:21:38 +0900

    モデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :purchases
 
   with_options presence: true do
+    validates :nickname, :birthday
     validates :email, format: { with: /@./, message: "このメールアドレスは使用できません。"}
     validates :password, format: { with: /\A[a-zA-Z0-9]+\z{6,}/, message: "is invalid. Input full-width characters."}
     validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width katakana characters."}
